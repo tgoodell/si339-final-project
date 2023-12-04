f97a19bdd5d49e29f6a799242f2fcef9b36ad190
tgoodell <github@tgoodell.com>
2023-12-04 01:28:18 -0500

    Margin around default container on mobile

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -2,6 +2,6 @@
     <footer>
         <hr>
         <p>This Site is not officially affiliated with the <a href="https://asmsa.org">Arkansas School for Math, Science, and the Arts</a></p>
-        <p>An ASMSA Fan Site | SI 339 Final Project | &copy; 2023</p>
+        <p><a href="/">Simple Blog CMS</a> | SI 339 Final Project | &copy; 2023</p>
     </footer>
 </template>
