8508d35b9d3e9c9b8c15b77739eae1d1b8307cf4
tgoodell <github@tgoodell.com>
2023-12-04 01:40:48 -0500

    Removed footer disclaimer

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,7 +1,6 @@
 <template>
     <footer>
         <hr>
-        <p>This Site is not officially affiliated with the <a href="https://asmsa.org">Arkansas School for Math, Science, and the Arts</a></p>
         <p><a href="/">Simple Blog CMS</a> | SI 339 Final Project | &copy; 2023</p>
     </footer>
 </template>
